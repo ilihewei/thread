5fc4df7f3c8c0e96b4c509f66189ef454fb26e62
lihewei <1968895380@qq.com>
2020-08-12 13:27:07 +0800

    并发包的lock各种用法

--- src/com/lihewei/concurrency4/MyTest2.java
+++ src/com/lihewei/concurrency4/MyTest2.java
@@ -6,7 +6,12 @@ package com.lihewei.concurrency4;
  *
  * 从jdk 5开始 并发包提供了lock，condition（wait 与signal、signalAll）来实现多个线程之间的协调与通信，整个过程都是由开发者
  * 来控制的，而且相比较传统方式，功能也更加强大
- *
+ * @author:lihewei
+ * Thread.sleep与awit(或是object的wait方法)的本质区别，sleep方法本质上不会释放锁，而await会释放锁，并且在signal后，还需要
+ * 重新获得锁才能继续执行，该行为与object方法完全一致
  */
 public class MyTest2 {
+
+
+
 }
