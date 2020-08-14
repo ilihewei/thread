d4430e7e2fe9c8e381dbf07f56f376a092123160
lihewei <1968895380@qq.com>
2020-08-14 12:23:12 +0800

    countdownlatch

---
+++ src/com/lihewei/concurrency5/MyTest1.java
@@ -0,0 +1,34 @@
+package com.lihewei.concurrency5;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.stream.IntStream;
+
+/**
+ * @author  lihewei
+ * CountDownLatch
+ */
+public class MyTest1 {
+    public static void main(String[] args) {
+        CountDownLatch countDownLatch=new CountDownLatch(3);
+        IntStream.range(0,3).forEach(i->new Thread(()->{
+            try {
+                Thread.sleep(1000);
+                System.out.println("hello world");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }finally {
+                countDownLatch.countDown();
+            }
+        }).start());
+        System.out.println("启动自线程完毕");
+
+        try {
+            //减少到0才会调用这个方法
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("主线程执行完毕");
+    }
+
+}
