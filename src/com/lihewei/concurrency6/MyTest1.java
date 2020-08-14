8c0a09a756d1b5799be9cca541ba38bf481c45e8
lihewei <1968895380@qq.com>
2020-08-14 15:26:11 +0800

    cas的实例

---
+++ src/com/lihewei/concurrency6/MyTest1.java
@@ -0,0 +1,16 @@
+package com.lihewei.concurrency6;
+
+/**
+ *
+ */
+public class MyTest1 {
+    private int count;
+
+    public int getCount() {
+        return count;
+    }
+
+    private  void increaseCount(){
+        ++this.count;
+    }
+}
